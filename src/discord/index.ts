f81c288db5e2c45d5b2a66fcb0474d6cc092befa
Thomas Kottke <t.kottke90@gmail.com>
2025-08-27 23:41:20 -0500

    get bot connected

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -2,6 +2,15 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 import { Logger } from '../utils/logging';
 
+const intents = [
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMessages,
+  GatewayIntentBits.MessageContent,
+  GatewayIntentBits.GuildMembers,
+  GatewayIntentBits.DirectMessages,
+  GatewayIntentBits.DirectMessageTyping
+]
+
 export default function initialize() {
   const logger = new Logger('Discord');
   logger.info('Initializing Discord...');
@@ -14,11 +23,19 @@ export default function initialize() {
   }
 
   // Create a new client instance
-  const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+  const client = new Client({ intents });
 
   // When the client is ready, run this code (only once).
   client.once(Events.ClientReady, readyClient => {
     logger.info(`Ready! Logged in as ${readyClient.user.tag}`);
+  });
+
+  client.on(Events.MessageCreate, message => {
+    logger.info(`Message from ${message.author.username}: ${message.toJSON()}`);
+  });
+
+  client.on(Events.MessageUpdate, message => {
+    logger.info(`Message from ${message?.author?.tag}: ${message.content}`);
   });
 
   // Log in to Discord with your client's token
